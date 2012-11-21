d300064b5c2a342d8f08b34bb2f4bb7501409616
AndrewAnderson <andrew.anderson@gmail.com>
2012-11-21 13:24:14 -0500

    Implemented Write for MCB Parser.

--- Cookbookology.Formats/MyCookbook/IO/MyCookbookParser.cs
+++ Cookbookology.Formats/MyCookbook/IO/MyCookbookParser.cs
@@ -6,11 +6,14 @@ using System.Text;
 using System.IO;
 using System.Xml.Serialization;
 using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
 
 namespace Cookbookology.Formats.MyCookbook.IO
 {
     public class MyCookbookParser : IParser
     {
+        const string mcbFileName = "BackupMyCookBook.xml";
+
         public Cookbookology.Domain.Cookbook Read(Stream inputStream)
         {
             if (inputStream == null) throw new ArgumentNullException("inputStream");
@@ -31,9 +34,36 @@ namespace Cookbookology.Formats.MyCookbook.IO
             return cookbook;
         }
 
+        // See this link for details on zipping using SharpZipLib:  https://github.com/icsharpcode/SharpZipLib/wiki/Zip-Samples#wiki-anchorCreate
         public void Write(Cookbookology.Domain.Cookbook cookbook, Stream outputStream)
         {
-            throw new NotImplementedException();
+            if (cookbook == null) throw new ArgumentNullException("cookbook");
+            if (outputStream == null) throw new ArgumentNullException("outputStream");
+
+            var converter = new MyCookbookConverter();
+            var mcb = converter.ConvertFromCommon(cookbook);
+
+            var ms = new MemoryStream();
+            var s = new XmlSerializer(typeof(Cookbook));
+            s.Serialize(ms, mcb);
+            ms.Position = 0; // reset to the start so that we can write the stream
+
+            // Add the cookbook as a single compressed file in a Zip
+            using (var zipStream = new ZipOutputStream(outputStream))
+            {
+                zipStream.SetLevel(3); // compression
+                zipStream.UseZip64 = UseZip64.Off; // not compatible with all utilities and OS (WinXp, WinZip8, Java, etc.)
+
+                var entry = new ZipEntry(mcbFileName);
+                entry.DateTime = DateTime.Now;
+
+                zipStream.PutNextEntry(entry);
+                StreamUtils.Copy(ms, zipStream, new byte[4096]);
+                zipStream.CloseEntry();
+
+                zipStream.IsStreamOwner = false; // Don't close the outputStream (parameter)
+                zipStream.Close();
+            }           
         }
     }
 }
--- Tests/Cookbookology.Domain.Test/MyCookbook/IO/MyCookbookParserTest.cs
+++ Tests/Cookbookology.Domain.Test/MyCookbook/IO/MyCookbookParserTest.cs
@@ -16,7 +16,7 @@ namespace Cookbookology.Formats.Test.MyCookbook.IO
 
         [TestMethod]
         [DeploymentItem(@"Input\MyCookBookSample.mcb", "Test Data")]
-        public void TryReadTest()
+        public void ReadTest()
         {
             // arrange
             var parser = new MyCookbookParser();
@@ -34,6 +34,50 @@ namespace Cookbookology.Formats.Test.MyCookbook.IO
 
             // assert
             Assert.AreEqual(15, cb.Recipes.Count);
+        }
+
+        [TestMethod]
+        public void WriteTest()
+        {
+            // arrange
+            string fileName = @"MyCookbookParserWriteTest.mcb";
+            var cookbook = new Cookbookology.Domain.Cookbook { Version = "4" };
+
+            cookbook.Recipes.Add
+            (
+                new Cookbookology.Domain.Recipe
+                {
+                    AdditionalComments = "These taste great on a cold morning!",
+                    Categories = new List<string> { "Breakfast", "French" },
+                    CookingTime = "20 min",
+                    ImagePath = @"/mnt/sdcard/MyCookBook/images/crepes.png",
+                    Ingredients = new List<string> { "1 1/2 c. flour", "2 c. milk", "2 eggs", "1 1/2 tbsp. oil", "1 tbsp. sugar", "Little salt melted" },
+                    Instructions = @"Pour the milk into the flour. Stir.
+Add the oil, the beaten eggs and the sugar. Stir again.
+Let the batter rest for 2 hours. The batter must be fluid. If not, add a little more milk.
+
+Take a frying pan, oil it and pour a small amount of batter and spread it on the bottom. Cook it on one side, then the other.
+The ""French Crepes"" must be very thin.
+You can put butter or sugar or jelly or melted chocolate on them.",
+                    PreparationTime = "10 min",
+                    Servings = "10 crepes",
+                    SourceUri = "http://test.com",
+                    Title = "Crepes",
+                }
+            );
+
+            var parser = new MyCookbookParser();
+            using (var outputFileStream = File.OpenWrite(fileName))
+            {
+
+                // act
+                parser.Write(cookbook, outputFileStream);
+
+                // assert
+                var fi = new FileInfo(fileName);
+                Assert.IsTrue(fi.Exists);
+                Assert.IsTrue(fi.Length > 52); // 52 is the length of a ZIP with an empty file
+            }
         }
     }
 }
