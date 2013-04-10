af34080a74f80a40cef99a21fd82a1377ffab318
Andrew Anderson <andrew.anderson@gmail.com>
2013-04-10 19:42:30 -0400

    Refinements to the gui layout

---
+++ Cookbookology/CookbookMain.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace Cookbookology
+{
+	partial class CookbookMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CookbookMain));
+            this.recipeTree = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.sourceUriTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.commentsTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.instructionsTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ingredientsTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cookTimeTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.prepTimeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.recipeGroupListBox = new System.Windows.Forms.ListBox();
+            this.menuToolStrip = new System.Windows.Forms.ToolStrip();
+            this.openButton = new System.Windows.Forms.ToolStripButton();
+            this.saveButton = new System.Windows.Forms.ToolStripButton();
+            this.newFileButton = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.imageTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // recipeTree
+            // 
+            this.recipeTree.Location = new System.Drawing.Point(12, 63);
+            this.recipeTree.Name = "recipeTree";
+            this.recipeTree.Size = new System.Drawing.Size(190, 592);
+            this.recipeTree.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.browseButton);
+            this.groupBox1.Controls.Add(this.imageTextBox);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.sourceUriTextBox);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.commentsTextBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.instructionsTextBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.ingredientsTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.quantityTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cookTimeTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.prepTimeTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.titleTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(215, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(712, 619);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recipe";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 580);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Image:";
+            // 
+            // sourceUriTextBox
+            // 
+            this.sourceUriTextBox.Location = new System.Drawing.Point(79, 545);
+            this.sourceUriTextBox.Name = "sourceUriTextBox";
+            this.sourceUriTextBox.Size = new System.Drawing.Size(614, 20);
+            this.sourceUriTextBox.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 548);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Source Link:";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(387, 70);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 19);
+            this.checkedListBox1.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(328, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Categories:";
+            // 
+            // commentsTextBox
+            // 
+            this.commentsTextBox.Location = new System.Drawing.Point(13, 124);
+            this.commentsTextBox.Multiline = true;
+            this.commentsTextBox.Name = "commentsTextBox";
+            this.commentsTextBox.Size = new System.Drawing.Size(680, 93);
+            this.commentsTextBox.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Comments:";
+            // 
+            // instructionsTextBox
+            // 
+            this.instructionsTextBox.Location = new System.Drawing.Point(317, 237);
+            this.instructionsTextBox.Multiline = true;
+            this.instructionsTextBox.Name = "instructionsTextBox";
+            this.instructionsTextBox.Size = new System.Drawing.Size(376, 298);
+            this.instructionsTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Directions:";
+            // 
+            // ingredientsTextBox
+            // 
+            this.ingredientsTextBox.Location = new System.Drawing.Point(10, 237);
+            this.ingredientsTextBox.Multiline = true;
+            this.ingredientsTextBox.Name = "ingredientsTextBox";
+            this.ingredientsTextBox.Size = new System.Drawing.Size(285, 298);
+            this.ingredientsTextBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ingredients:";
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(573, 69);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(120, 20);
+            this.quantityTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(513, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Servings:";
+            // 
+            // cookTimeTextBox
+            // 
+            this.cookTimeTextBox.Location = new System.Drawing.Point(225, 69);
+            this.cookTimeTextBox.Name = "cookTimeTextBox";
+            this.cookTimeTextBox.Size = new System.Drawing.Size(89, 20);
+            this.cookTimeTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cook time:";
+            // 
+            // prepTimeTextBox
+            // 
+            this.prepTimeTextBox.Location = new System.Drawing.Point(67, 69);
+            this.prepTimeTextBox.Name = "prepTimeTextBox";
+            this.prepTimeTextBox.Size = new System.Drawing.Size(86, 20);
+            this.prepTimeTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prep time:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title:";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(67, 27);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(626, 20);
+            this.titleTextBox.TabIndex = 0;
+            // 
+            // recipeGroupListBox
+            // 
+            this.recipeGroupListBox.AllowDrop = true;
+            this.recipeGroupListBox.FormattingEnabled = true;
+            this.recipeGroupListBox.Location = new System.Drawing.Point(13, 40);
+            this.recipeGroupListBox.Name = "recipeGroupListBox";
+            this.recipeGroupListBox.Size = new System.Drawing.Size(189, 17);
+            this.recipeGroupListBox.TabIndex = 2;
+            // 
+            // menuToolStrip
+            // 
+            this.menuToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFileButton,
+            this.openButton,
+            this.saveButton});
+            this.menuToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuToolStrip.Name = "menuToolStrip";
+            this.menuToolStrip.Size = new System.Drawing.Size(944, 25);
+            this.menuToolStrip.TabIndex = 3;
+            this.menuToolStrip.Text = "toolStrip1";
+            // 
+            // openButton
+            // 
+            this.openButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+            this.openButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(23, 22);
+            this.openButton.Text = "Open";
+            this.openButton.ToolTipText = "Open file";
+            // 
+            // saveButton
+            // 
+            this.saveButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveButton.Image = ((System.Drawing.Image)(resources.GetObject("saveButton.Image")));
+            this.saveButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(23, 22);
+            this.saveButton.Text = "Save";
+            this.saveButton.ToolTipText = "Save file";
+            // 
+            // newFileButton
+            // 
+            this.newFileButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.newFileButton.Image = ((System.Drawing.Image)(resources.GetObject("newFileButton.Image")));
+            this.newFileButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newFileButton.Name = "newFileButton";
+            this.newFileButton.Size = new System.Drawing.Size(23, 22);
+            this.newFileButton.Text = "New";
+            this.newFileButton.ToolTipText = "New file";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // imageTextBox
+            // 
+            this.imageTextBox.Location = new System.Drawing.Point(79, 577);
+            this.imageTextBox.Name = "imageTextBox";
+            this.imageTextBox.ReadOnly = true;
+            this.imageTextBox.Size = new System.Drawing.Size(512, 20);
+            this.imageTextBox.TabIndex = 19;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(597, 577);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(96, 23);
+            this.browseButton.TabIndex = 20;
+            this.browseButton.Text = "Browse...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // CookbookMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(944, 665);
+            this.Controls.Add(this.menuToolStrip);
+            this.Controls.Add(this.recipeGroupListBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.recipeTree);
+            this.Name = "CookbookMain";
+            this.Text = "Cookbookology";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuToolStrip.ResumeLayout(false);
+            this.menuToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView recipeTree;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox quantityTextBox;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox cookTimeTextBox;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox prepTimeTextBox;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox titleTextBox;
+		private System.Windows.Forms.TextBox ingredientsTextBox;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox instructionsTextBox;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox commentsTextBox;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox sourceUriTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox recipeGroupListBox;
+        private System.Windows.Forms.ToolStrip menuToolStrip;
+        private System.Windows.Forms.ToolStripButton openButton;
+        private System.Windows.Forms.ToolStripButton saveButton;
+        private System.Windows.Forms.ToolStripButton newFileButton;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox imageTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+	}
+}
+
--- Cookbookology/CookbookMain.cs
+++ Cookbookology/CookbookMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Cookbookology
 {
-	public partial class Form1 : Form
+	public partial class CookbookMain : Form
 	{
-		public Form1()
+		public CookbookMain()
 		{
 			InitializeComponent();
 		}
